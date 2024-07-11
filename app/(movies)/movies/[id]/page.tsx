d11e5830f85739a6cf7efa3c6aca9b63d3be7dfb
colorkite10 <colorkite10@gmail.com>
2024-07-11 13:08:15 +0900

    dynamic metadata using generateMetadata

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,12 +1,19 @@
 import { Suspense } from "react";
-import MovieInfo from "../../../../components/movie-info";
+import MovieInfo, { getMovie } from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
 
-export default function MovieDetail({
-  params: { id },
-}: {
+interface Params {
   params: { id: string };
-}) {
+}
+
+export async function generateMetadata({ params: { id } }: Params) {
+  const movie = await getMovie(id);
+  return {
+    title: movie.title,
+  };
+}
+
+export default function MovieDetail({ params: { id } }: Params) {
   return (
     <div>
       <Suspense fallback={<h1>Loading movie info</h1>}>
